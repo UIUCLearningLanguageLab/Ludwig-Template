4cd83724a498d0d1e36c6aa58d7239f3c374721f
Philip Huebner <info@philhuebner.com>
2021-02-20 07:28:51 -0600

    Update params.py

--- src/params.py
+++ src/params.py
@@ -1,3 +1,13 @@
+"""
+This file defines the hyper-parameters used for each Ludwig job.
+By collecting multiple values per hyper-parameter in a list in param2requests, 
+Ludwig will run jobs corresponding to all combination of hyper-parameter values. 
+Any hyper-parameter not overwritten by param2requests will be assigned its default value using param2default.
+
+Note: Ludwig relies on the dictionaries below to be named as-is. Do not rename them.
+
+"""
+
 # will submit 3*2=6 jobs, each using a different learning rate and "configuration"
 param2requests = {
     'learning_rate': [0.1, 0.2, 0.3],
